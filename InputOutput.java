1f8dfd7de2c95f293d59cd4895e9f17f4015ac5e
aqshalms <aqshalms48@gmail.com>
2020-07-06 10:02:26 +0800

    InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class InputOutput{
  public static void main(String [] args){
+  Scanner scanner = new Scanner(System.in);
+  String nama;
+  String kelas;
 
+  System.out.print("Masukkan nama anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+  System.out.print("Masukkan kelas anda: ");
+  kelas = scanner.nextLine();
+  System.out.println("Selamat datang di kelas "+kelas);
  }
 }
